788369d92900443d373f8ab8777bc5b67b4f33c4
HelpfulStranger999 <25759101+HelpfulStranger999@users.noreply.github.com>
2024-06-17 09:49:17 -0500

    Refactors stream schedule into classes

--- ScheduleGenerator/Program.cs
+++ ScheduleGenerator/Program.cs
@@ -8,6 +8,8 @@ using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
+using ScheduleGenerator;
+
 FontFamily GetFontFamily()
 {
     var collection = new FontCollection();
@@ -19,9 +21,10 @@ FontFamily GetFontFamily()
     return collection.Add(stream!);
 }
 
-Image<Rgba32> GenerateScheduleImage(StreamSchedule[] streamSchedule, FontFamily fontFamily)
+Image<Rgba32> GenerateScheduleImage(Schedule schedule, FontFamily fontFamily)
 {
-    var image = new Image<Rgba32>(400, 140 + streamSchedule.Length * 110, Rgba32.ParseHex("#F4EEDA"));
+    if (schedule.IsEmpty) throw new InvalidOperationException("Cannot generate a schedule without streams");
+    var image = new Image<Rgba32>(400, 140 + schedule.Count * 110, Rgba32.ParseHex("#F4EEDA"));
 
     var largeFont = fontFamily.CreateFont(50, FontStyle.Regular);
     var smallFont = fontFamily.CreateFont(20, FontStyle.Regular);
@@ -31,8 +34,8 @@ Image<Rgba32> GenerateScheduleImage(StreamSchedule[] streamSchedule, FontFamily 
     var textColor = new Color(Rgba32.ParseHex("#000000"));
     var pen = new SolidPen(textColor, 10);
 
-    var firstDate = streamSchedule[0].Date;
-    var lastDate = streamSchedule[^1].Date;
+    var firstDate = (DateOnly)schedule.StartDate!;
+    var lastDate = (DateOnly)schedule.EndDate!;
     var sameMonth = firstDate.Month == lastDate.Month;
 
     var dateRange = new StringBuilder();
@@ -50,6 +53,7 @@ Image<Rgba32> GenerateScheduleImage(StreamSchedule[] streamSchedule, FontFamily 
         dateRange.Append(lastDate.ToString("MMM d"));
     }
 
+    var streams = schedule.ToList();
     image.Mutate(context =>
     {
         context.DrawText($"Weekly Schedule\nfor {dateRange}", largeFont, textColor,
@@ -57,10 +61,10 @@ Image<Rgba32> GenerateScheduleImage(StreamSchedule[] streamSchedule, FontFamily 
 
         context.DrawLine(pen, new PointF(160, 140), new PointF(160, image.Height));
 
-        for (int i = 0; i < streamSchedule.Length; i++)
+        for (int i = 0; i < schedule.Count; i++)
         {
             int xPos = 140 + i * 110;
-            var schedule = streamSchedule[i];
+            var schedule = streams[i];
 
             context.DrawLine(pen, new PointF(0, xPos), new PointF(400, xPos));
 
@@ -83,12 +87,12 @@ Image<Rgba32> GenerateScheduleImage(StreamSchedule[] streamSchedule, FontFamily 
     return image;
 }
 
-StreamSchedule CreateScheduleDay(DateOnly currentDate)
+ScheduledStream CreateScheduleDay(DateOnly currentDate)
 {
     var willStream = Prompt.Confirm($"Will you be streaming on {currentDate.ToLongDateString()}?");
     if (!willStream)
     {
-        return new StreamSchedule(null, currentDate, null);
+        return new ScheduledStream(currentDate);
     }
     else
     {
@@ -99,17 +103,21 @@ StreamSchedule CreateScheduleDay(DateOnly currentDate)
         }
 
         var time = Prompt.Input<TimeOnly>("When will you stream?");
-        return new StreamSchedule(title, currentDate, time);
+        return new ScheduledStream(currentDate)
+        {
+            Title = title,
+            Time = time
+        };
     }
 }
 
 var firstDate = Prompt.Input<DateOnly>("What is the first date of the stream schedule?");
-var scheduleList = new List<StreamSchedule>();
+var schedule = new Schedule();
 
 DateOnly currentDate = firstDate;
 while (true)
 {
-    scheduleList.Add(CreateScheduleDay(currentDate));
+    schedule.Add(CreateScheduleDay(currentDate));
     if (Prompt.Confirm("Add another day to the schedule?"))
     {
         currentDate = currentDate.AddDays(1);
@@ -121,11 +129,10 @@ while (true)
 }
 
 
-var image = GenerateScheduleImage(scheduleList.ToArray(), GetFontFamily());
 
 var fileLocation = Prompt.Input<string>("Schedule generated. Would you like the schedule saved?");
 
 await image.SaveAsPngAsync(fileLocation);
+var image = GenerateScheduleImage(schedule, GetFontFamily());
 
 
-record StreamSchedule(string? Title, DateOnly Date, TimeOnly? Time);
---
+++ ScheduleGenerator/StreamSchedule.cs
@@ -0,0 +1,82 @@
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ScheduleGenerator;
+
+public class Schedule
+{
+    public bool IsEmpty => Streams.Count <= 0;
+    public int Count => (EndDate?.DayNumber - StartDate?.DayNumber) ?? 0;
+
+    [MemberNotNullWhen(false, nameof(IsEmpty))]
+    public DateOnly? StartDate { get; private set; } = null;
+
+    [MemberNotNullWhen(false, nameof(IsEmpty))]
+    public DateOnly? EndDate { get; private set; } = null;
+
+    private Dictionary<DateOnly, ScheduledStream> Streams { get; } = [];
+
+    public ScheduledStream this[DateOnly date]
+    {
+        get => Streams[date];
+    }
+
+    public void Add(ScheduledStream stream)
+    {
+        Streams.Add(stream.Date, stream);
+
+        if ((StartDate is null) || (stream.Date < StartDate))
+        {
+            StartDate = stream.Date;
+        }
+
+        if ((EndDate is null) || (stream.Date > EndDate))
+        {
+            EndDate = stream.Date;
+        }
+    }
+
+    public ImmutableList<ScheduledStream> ToList()
+    {
+        if (StartDate is null)
+        {
+            return [];
+        }
+
+        var streams = new List<ScheduledStream>();
+
+        for (var date = (DateOnly)StartDate; date <= EndDate; date = date.AddDays(1))
+        {
+            if (Streams.TryGetValue(date, out var stream))
+            {
+                streams.Add(stream);
+            }
+            else
+            {
+                var newStream = new ScheduledStream(date);
+                Streams.Add(date, newStream);
+                streams.Add(newStream);
+            }
+        }
+
+        return streams.ToImmutableList();
+    }
+}
+
+public class ScheduledStream
+{
+    public bool DoesStream => Time != null;
+
+    [MemberNotNullWhen(true, nameof(DoesStream))]
+    public string? Title { get; set; } = null;
+
+    [MemberNotNullWhen(true, nameof(DoesStream))]
+    public TimeOnly? Time { get; set; }
+
+    public DateOnly Date { get; }
+
+    public ScheduledStream(DateOnly date)
+    {
+        Date = date;
+    }
+}
